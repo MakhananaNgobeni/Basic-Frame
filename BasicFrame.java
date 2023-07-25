25ef8fce029e7b924cfbf0b77e7c22bb99e6e2ea
MakhananaNgobeni <140527830+MakhananaNgobeni@users.noreply.github.com>
2023-07-25 16:55:26 +0200

    Add files via upload

---
+++ BasicFrame.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframe;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author oddette
+ */
+public class BasicFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyFirstFrame myFrame = new MyFirstFrame();
+    }
+    
+}
